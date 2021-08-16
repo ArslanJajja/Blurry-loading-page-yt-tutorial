e1df0412bffc881d989f89b0d07154bd63b14a18
Arslan Jajja <arslanjajja1@gmail.com>
2021-08-16 08:47:52 +0500

    blurry loading page

---
+++ script.js
@@ -0,0 +1,20 @@
+const bg=document.querySelector('.bg')
+const loadText=document.querySelector('.loading-text')
+
+let load=0
+
+let int=setInterval(blurring,30)
+
+function blurring(){
+    load++; 
+    if(load>99){
+        clearInterval(int)
+    }
+    loadText.innerText=`${load}%`
+    loadText.style.opacity=scale(load,0,100,1,0)
+    bg.style.filter=`blur(${scale(load,0,100,30,0)}px)`
+}
+
+function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
